703b30b5b4bac97cf7c201bdc59562b793191ffd
Priscilla Leyva <pleyva.co89@gmail.com>
2020-06-17 01:05:04 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -14,7 +14,13 @@
 
 def speak_to_grandma(message)
   if message == "I LOVE YOU GRANDMA!"
-    return
+    return "I LOVE YOU TOO PUMPKIN!"
+  elsif message != message.upcase
+    return "HUH?! SPEAK UP, SONNY!"
+  else
+    return "NO, NOT SINCE 1938!"
+  end
+end
 
 
   
